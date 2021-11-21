87c9a468026af7575840dad938a771e89bbbf41c
vivek <vivekbk1997@gmail.com>
2021-11-22 00:26:15 +0530

    changed code in daysBwDates.js

--- daysBwDates.js
+++ daysBwDates.js
@@ -16,36 +16,45 @@ else days+=365;
 
 
 for(i=m1+1;i<=12;i++){
+if(i==2){
 if(checkLeap(y1) == true){
-if(i==2) days+=29;
+ days+=29;
 }
 else {
-if(i==2) days+=28;
+days+=28;
 }
-if(i==4||i==6||i==9||i==11)
+}
+else if(i==4||i==6||i==9||i==11)
 days += 30;
 else days += 31;
 }
 
 
 for(i=m2-1;i>=1;i--){
+if(i==2){
 if(checkLeap(y2) == true){
-if(i==2) days+=29;
+ days+=29;
 }
 else {
-if(i==2) days+=28;
+days+=28;
 }
-if(i==4||i==6||i==9||i==11)
+}
+else if(i==4||i==6||i==9||i==11)
 days += 30;
-if(i==1||i==3||i==5||i==7||i==8||i==10||i==12)
-days += 31;
+else days += 31;
 }
 
 
-if(m1==4||m1==6||m1==9||m1==11)
+if(m1==2){
+if(checkLeap(y1)==true){
+days += 29;
+}
+else days += 28;
+}
+else if(m1==4||m1==6||m1==9||m1==11)
 days += (30 - d1);
-if(m1==1||m1==3||m1==5||m1==7||m1==8||m1==10||m1==12)
-days += (31 - d1);
+else days += (31 - d1);
+
 days+=d2;
 
 return days;
