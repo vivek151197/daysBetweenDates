06ef5c414a7f5c188219c5a6c365d5934ef433ec
vivek <vivekbk1997@gmail.com>
2021-11-22 11:36:00 +0530

    added comments to daysBwDatesAlternate.js

--- daysBwDatesAlternate.js
+++ daysBwDatesAlternate.js
@@ -7,7 +7,7 @@ y1 = Math.floor(a1/10000),
 y2 = Math.floor(a2/10000);
 let daysTilld1 = 0, daysTilld2 = 0;
 
-
+//find the days till d1 from 0 AD.
 daysTilld1 += (y1-1)*365 + leapYears(y1-1);
 for(let i=0;i<m1;i++){
 daysTilld1 += daysInMonth[i];
@@ -17,7 +17,7 @@ if(m1>2) daysTilld1 += 1;
 }
 daysTilld1 += d1;
 
-
+//find the days till d2 from 0 AD.
 daysTilld2 += (y2-1)*365 + leapYears(y2-1);
 for(let i=0;i<m2;i++){
 daysTilld2 += daysInMonth[i];
@@ -31,15 +31,15 @@ daysTilld2 += d2;
 return (daysTilld2 - daysTilld1);
 }
 
-
+//Array to store days in each month
 let daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
 
-
+//function to count number of leap years till the given year from 0 AD.
 function leapYears(yr){
 return (Math.floor(yr/4) - Math.floor(yr/400) + Math.floor(yr/100))
 }
 
-
+//function to check if the given year is leap.
 function checkLeap(yr){
 if(yr%400==0 || (yr%4==0 && yr%100!=0)) {
 return true;
