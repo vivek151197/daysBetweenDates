b0cacb28feb27a998e2abef64fb214c64eed902c
vivek <vivekbk1997@gmail.com>
2021-11-22 19:10:27 +0530

    added recursion version and indented the old version

--- daysBwDates.js
+++ daysBwDates.js
@@ -1,74 +1,73 @@
-function numberOfDays(a1,a2){
-let d1 = a1%100,
-d2 = a2%100,
-m1 = Math.floor((a1%10000)/100),
-m2 = Math.floor((a2%10000)/100),
-y1 = Math.floor(a1/10000),
-y2 = Math.floor(a2/10000);
-let days = 0;
+function numberOfDays(a1,a2) {
+	let d1 = a1%100,
+	d2 = a2%100,
+	m1 = Math.floor((a1%10000)/100),
+	m2 = Math.floor((a2%10000)/100),
+	y1 = Math.floor(a1/10000),
+	y2 = Math.floor(a2/10000);
+	let days = 0;
+// add up the days in the between years
+	for(let i=y1+1; i<y2; i++) { 
+		if(checkLeap(i) == true){
+		days+=366;
+		}
+		else days+=365;
+	}
 
-for(let i=y1+1; i<y2; i++){ // add up the days in the between years
-if(checkLeap(i) == true){
-days+=366;
-}
-else days+=365;
-}
+// add up the days in months of the given starting year
+	for(i=m1+1;i<=12;i++) { 
+		if(i==2) {
+			if(checkLeap(y1) == true) {
+				days+=29;
+			}
+			else {
+				days+=28;
+			}
+		}
+		else if(i==4||i==6||i==9||i==11)
+			days += 30;
+		else days += 31;
+	}
 
+//add up the days in months of the given ending year
+	for(i=m2-1;i>=1;i--){
+		if(i==2){
+			if(checkLeap(y2) == true){
+ 				days+=29;
+			}
+			else {
+				days+=28;
+			}
+		}
+		else if(i==4||i==6||i==9||i==11) days += 30;
+		else days += 31;
+	}
 
-for(i=m1+1;i<=12;i++){ // add up the days in months of the given starting year
-if(i==2){
-if(checkLeap(y1) == true){
- days+=29;
-}
-else {
-days+=28;
-}
-}
-else if(i==4||i==6||i==9||i==11)
-days += 30;
-else days += 31;
-}
-
-
-for(i=m2-1;i>=1;i--){ //add up the days in months of the given ending year
-if(i==2){
-if(checkLeap(y2) == true){
- days+=29;
-}
-else {
-days+=28;
-}
-}
-else if(i==4||i==6||i==9||i==11)
-days += 30;
-else days += 31;
-}
-
-
-if(m1==2){ // add up the remaining days in the starting month from the day given
-if(checkLeap(y1)==true){
-days += 29;
-}
-else days += 28;
-}
-else if(m1==4||m1==6||m1==9||m1==11)
-days += (30 - d1);
-else days += (31 - d1);
-
-days+=d2; // add up the days in the ending month
+// add up the remaining days in the starting month from the day given
+	if(m1==2){
+		if(checkLeap(y1)==true){
+			days += 29;
+		}
+		else days += 28;
+	}
+	else if(m1==4||m1==6||m1==9||m1==11)
+		days += (30 - d1);
+	else days += (31 - d1);
+// add up the days in the ending month
+	days+=d2; 
 
 return days;
 }
 
-
-function checkLeap(yr){ // function to check if it is a leap year
-if(yr%400==0 || (yr%4==0 && yr%100!=0)) {
-return true;
+// function to check if it is a leap year
+function checkLeap(yr){ 
+	if(yr%400==0 || (yr%4==0 && yr%100!=0)) {
+		return true;
+	}
+	else return false;
 }
-else return false;
-}
 
 
-let bday = 19971115, today = 20211121;
+let bday = 19971115, today = 20211122;
 let daysBetweenDates = numberOfDays(bday,today);
 console.log(daysBetweenDates);
---
+++ recursiondays.js
@@ -0,0 +1,31 @@
+let count = 0;
+function days(d1, m1, y1, d2, m2, y2) {
+	if(m1 == m2 && y1 == y2) {
+	  count += (d2 - d1);
+	  return count;
+	}
+
+	else{
+		if(m1 == 2) {
+			if(y1 % 400 == 0 || (y1 % 100 != 0 && y1 % 4 == 0)) {
+ 			count += 29;
+		}
+			else count += 28;
+		}
+		else if(m1 == 4 || m1 == 6 || m1 == 9 || m1 == 11) {
+			count += 30;
+		}
+		else count += 31;
+
+		m1++;
+		
+		if(m1 == 13) { 
+			m1 = 1;
+			y1++;
+		}
+	}
+return days(d1, m1, y1, d2, m2, y2, count);
+}
+
+let numberOfDays = days(15, 11, 1997, 22, 11, 2021);
+console.log(numberOfDays);
