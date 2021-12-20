2b59d04ef293407f0abf4679ae6c26e708f4a3c9
vivek <vivekbk1997@gmail.com>
2021-12-20 10:12:56 +0530

    indentation fixed

--- daysBwDatesAlternate.js
+++ daysBwDatesAlternate.js
@@ -1,53 +1,52 @@
-function numberOfDays(a1,a2){
-let d1 = a1%100,
-d2 = a2%100,
-m1 = Math.floor((a1%10000)/100),
-m2 = Math.floor((a2%10000)/100),
-y1 = Math.floor(a1/10000),
-y2 = Math.floor(a2/10000);
-let daysTilld1 = 0, daysTilld2 = 0;
+function numberOfDays (a1, a2) {
+  let d1 = a1 % 100,
+    d2 = a2 % 100,
+    m1 = Math.floor((a1 % 10000) / 100),
+    m2 = Math.floor((a2 % 10000) / 100),
+    y1 = Math.floor(a1 / 10000),
+    y2 = Math.floor(a2 / 10000)
+  let daysTilld1 = 0,
+    daysTilld2 = 0
 
-//find the days till d1 from 0 AD.
-daysTilld1 += (y1-1)*365 + leapYears(y1-1);
-for(let i=0;i<m1;i++){
-daysTilld1 += daysInMonth[i];
-}
-if(checkLeap(y1 == true)){
-if(m1>2) daysTilld1 += 1;
-}
-daysTilld1 += d1;
+  //find the days till d1 from 0 AD.
+  daysTilld1 += (y1 - 1) * 365 + leapYears(y1 - 1)
+  for (let i = 0; i < m1; i++) {
+    daysTilld1 += daysInMonth[i]
+  }
+  if (checkLeap(y1 == true)) {
+    if (m1 > 2) daysTilld1 += 1
+  }
+  daysTilld1 += d1
 
-//find the days till d2 from 0 AD.
-daysTilld2 += (y2-1)*365 + leapYears(y2-1);
-for(let i=0;i<m2;i++){
-daysTilld2 += daysInMonth[i];
-}
-if(checkLeap(y2 == true)){
-if(m2>2) daysTilld2 += 1;
-}
-daysTilld2 += d2;
+  //find the days till d2 from 0 AD.
+  daysTilld2 += (y2 - 1) * 365 + leapYears(y2 - 1)
+  for (let i = 0; i < m2; i++) {
+    daysTilld2 += daysInMonth[i]
+  }
+  if (checkLeap(y2 == true)) {
+    if (m2 > 2) daysTilld2 += 1
+  }
+  daysTilld2 += d2
 
-
-return (daysTilld2 - daysTilld1);
+  return daysTilld2 - daysTilld1
 }
 
 //Array to store days in each month
-let daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31];
+let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 //function to count number of leap years till the given year from 0 AD.
-function leapYears(yr){
-return (Math.floor(yr/4) - Math.floor(yr/400) + Math.floor(yr/100))
+function leapYears (yr) {
+  return Math.floor(yr / 4) - Math.floor(yr / 400) + Math.floor(yr / 100)
 }
 
 //function to check if the given year is leap.
-function checkLeap(yr){
-if(yr%400==0 || (yr%4==0 && yr%100!=0)) {
-return true;
-}
-else return false;
+function checkLeap (yr) {
+  if (yr % 400 == 0 || (yr % 4 == 0 && yr % 100 != 0)) {
+    return true
+  } else return false
 }
 
-
-let bday = 19971115, today = 20211121;
-let NumberOfDays = numberOfDays(bday,today);
-console.log(NumberOfDays);
+let bday = 19971115,
+  today = 20211121
+let NumberOfDays = numberOfDays(bday, today)
+console.log(NumberOfDays)
--- recursiondays.js
+++ recursiondays.js
@@ -1,31 +1,39 @@
-let count = 0;
-function days(d1, m1, y1, d2, m2, y2) {
-	if(m1 == m2 && y1 == y2) {
-	  count += (d2 - d1);
-	  return count;
-	}
+function days (d1, m1, y1, d2, m2, y2, count = 0) {
+  let changeInCount = 0
+  if (m1 == m2 && y1 == y2) {
+    return count + (d2 - d1)
+  }
 
-	else{
-		if(m1 == 2) {
-			if(y1 % 400 == 0 || (y1 % 4 == 0 && y1 % 100 != 0)) {
- 			count += 29;
-		}
-			else count += 28;
-		}
-		else if(m1 == 4 || m1 == 6 || m1 == 9 || m1 == 11) {
-			count += 30;
-		}
-		else count += 31;
-
-		m1++;
-		
-		if(m1 == 13) { 
-			m1 = 1;
-			y1++;
-		}
-	}
-return days(d1, m1, y1, d2, m2, y2, count);
+  changeInCount += daysInMonth(m1, y1)
+  m1++
+  if (m1 == 13) {
+    m1 = 1
+    y1++
+  }
+  return days(d1, m1, y1, d2, m2, y2, count + changeInCount)
 }
 
-let numberOfDays = days(15, 11, 1997, 22, 11, 2021);
-console.log(numberOfDays);
+function daysInMonth (month, year) {
+  if (month == 2) {
+    if (isLeapYear(year)) {
+      return 29
+    }
+    return 28
+  }
+
+  if (month == 4 || month == 6 || month == 9 || month == 11) {
+    return 30
+  }
+
+  return 31
+}
+
+function isLeapYear (year) {
+  if (year % 400 == 0) return true
+  if (year % 100 != 0) return false
+  if (year % 4 == 0) return true
+  return false
+}
+
+const numberOfDays = days(15, 11, 1997, 22, 11, 2021)
+console.log(numberOfDays)
